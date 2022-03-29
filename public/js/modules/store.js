367a4c6ce1eb62230d9f83c3d667a3188a5d58bb
Eric Tallant <eatallant@gmail.com>
2022-03-29 13:28:23 -0500

    initial setup of data persistence module

---
+++ public/js/modules/store.js
@@ -0,0 +1,7 @@
+export default function store() {
+  return {
+    setDefinitions: (definitions) =>
+      localStorage.setItem("definitions", JSON.stringify(definitions)),
+    getDefinitions: () => JSON.parse(localStorage.getItem("definitions")) || [],
+  };
+}
