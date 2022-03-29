b67443199975a787c651ea9893ba36aa398c7178
Eric Tallant <eatallant@gmail.com>
2022-03-29 13:27:23 -0500

    initial setup of animation files

---
+++ public/js/modules/animation.js
@@ -0,0 +1,13 @@
+/*
+ * ANIMATION UTILITIES
+ */
+
+// transition find a word button to input on click
+
+// transition find a word input back to button on blur
+
+// slide in part of speech buttons after successfull call to dictionaryAPI
+
+// dropdown filter options
+
+// dropdown sort options
