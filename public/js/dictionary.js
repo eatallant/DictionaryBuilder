ae4e886831afeaeee85f7172fb895f62d70f29a6
Eric Tallant <eatallant@gmail.com>
2022-03-31 09:52:29 -0500

    moved HTML rendering functions into renderHTML.js

--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -1,3 +1,5 @@
+import renderHTML from "./modules/renderHTML.js";
+
 ("use strict");
 console.log("page load successful");
 
@@ -11,7 +13,7 @@ const wordCounter = (() => {
   };
 })();
 
-const createNewDefinition = () => {
+window.createNewDefinition = () => {
   const word = prompt("Enter a word");
 
   if (!word) {
@@ -24,10 +26,10 @@ const createNewDefinition = () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data[0].meanings); // catch 404 error before any HTML rendering
-      renderNewEntryHTML(word);
+      renderHTML().newEntry(word);
       data[0].meanings.map((meaning) => {
         let definition = meaning.definitions[0].definition;
-        renderDefinitionHTML(definition, wordCounter.current());
+        renderHTML().definition(definition, wordCounter.current());
       });
       updateTitle();
       wordCounter.increment();
@@ -36,31 +38,6 @@ const createNewDefinition = () => {
       console.log(ex);
       alert("could not find " + word);
     });
-};
-
-const renderNewEntryHTML = (word) => {
-  let dictionaryBody = document.getElementById("dictionary-body");
-  let id = wordCounter.current();
-
-  //append header
-  let node = document.createElement("h3");
-  let textnode = document.createTextNode(word);
-  node.appendChild(textnode);
-  dictionaryBody.appendChild(node);
-
-  //append ordered list with unique id
-  node = document.createElement("ol");
-  node.setAttribute("id", id);
-  node.setAttribute("class", "definition-list");
-  dictionaryBody.appendChild(node);
-};
-
-const renderDefinitionHTML = (definition, id) => {
-  let node = document.createElement("LI");
-  node.setAttribute("class", "definition-list-item");
-  let textnode = document.createTextNode(definition);
-  node.appendChild(textnode);
-  document.getElementById(id).appendChild(node);
 };
 
 // track total words processed in document title
--- public/js/modules/renderHTML.js
+++ public/js/modules/renderHTML.js
@@ -1,1 +1,31 @@
-// move all HTML functions into this module
+// THIS IS JUST A PLACEHOLDER! Replace with GUID module before persistence
+let count = 0;
+
+export default function renderHTML() {
+  return {
+    newEntry: (word) => {
+      let dictionaryBody = document.getElementById("dictionary-body");
+      let id = count++; //this needs replaced with the GUID function when it's available
+
+      //append header
+      let node = document.createElement("h3");
+      let textnode = document.createTextNode(word);
+      node.appendChild(textnode);
+      dictionaryBody.appendChild(node);
+
+      //append ordered list with unique id
+      node = document.createElement("ol");
+      node.setAttribute("id", id);
+      node.setAttribute("class", "definition-list");
+      dictionaryBody.appendChild(node);
+    },
+
+    definition: (definition, id) => {
+      let node = document.createElement("LI");
+      node.setAttribute("class", "definition-list-item");
+      let textnode = document.createTextNode(definition);
+      node.appendChild(textnode);
+      document.getElementById(id).appendChild(node);
+    },
+  };
+}
