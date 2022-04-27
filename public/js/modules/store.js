c6caa0ef22a8f0d90d180095517de1e1c255d7d8
Eric Tallant <eatallant@gmail.com>
2022-04-27 13:30:31 -0500

    formatting update

--- public/js/modules/store.js
+++ public/js/modules/store.js
@@ -2,6 +2,7 @@ export default function store() {
   return {
     setDefinitions: (definitions) =>
       localStorage.setItem("definitions", JSON.stringify(definitions)),
+
     getDefinitions: JSON.parse(localStorage.getItem("definitions")) || [],
   };
 }
